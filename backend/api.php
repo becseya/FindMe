50b1f913443c46f41ac6a9b34b0075c9b336c0df
Akos Becsey <becsey.a@gmail.com>
2021-10-26 01:37:58 +0200

    Handle commands

--- backend/api.php
+++ backend/api.php
@@ -1,3 +1,39 @@
 <?php
-print('API is working');
+
+function fatal_error($description) {
+    http_response_code(400);
+    echo("$description\n");
+    exit;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+function users_list() {
+    echo("Listing users...\n");
+}
+
+function users_add() {
+    echo("User added!\n");
+}
+
+function users_remove() {
+    echo("User removed!\n");
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+switch($_GET['command']) {
+    case 'user-add':
+        users_add();
+        break;
+    case 'user-remove':
+        users_remove();
+        break;
+    case 'user-list':
+        users_list();
+        break;
+    default:
+        fatal_error("Unknown command");
+}
+
 ?>
