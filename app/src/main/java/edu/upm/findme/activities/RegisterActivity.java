3d881a77a40d1b836bbc650daf05fdca7e28e6f1
Akos Becsey <becsey.a@gmail.com>
2021-11-02 20:15:19 +0100

    AsyncHttpClient

--- app/src/main/java/edu/upm/findme/activities/RegisterActivity.java
+++ app/src/main/java/edu/upm/findme/activities/RegisterActivity.java
@@ -4,8 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import edu.upm.findme.R;
+import edu.upm.findme.utility.AsyncHttpClient;
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -16,6 +20,14 @@ public class RegisterActivity extends AppCompatActivity {
     }
 
     public void onTestBtn(View v) {
+        AsyncHttpClient client = new AsyncHttpClient();
 
+        RequestBody requestBody = new FormBody.Builder()
+                .add("name", "Jon Doe")
+                .add("phone", "+34-00-00-00-00")
+                .build();
+
+        client.post("https://findme.becsengo.hu/api.php?command=user-add", requestBody, (success, payload) ->
+                Toast.makeText(RegisterActivity.this, (success ? "OK " : "ERROR ") + payload, Toast.LENGTH_SHORT).show());
     }
 }
