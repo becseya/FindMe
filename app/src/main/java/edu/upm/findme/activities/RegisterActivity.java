d4231e3cfe0dc39bdd6cf43908a7d658cc745cc9
maryyyyyyaa <76183522+maryyyyyyaa@users.noreply.github.com>
2021-11-04 20:17:51 +0100

    GUI for registration

--- app/src/main/java/edu/upm/findme/activities/RegisterActivity.java
+++ app/src/main/java/edu/upm/findme/activities/RegisterActivity.java
@@ -1,16 +1,29 @@
 package edu.upm.findme.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import edu.upm.findme.R;
 
 public class RegisterActivity extends AppCompatActivity {
 
+    TextView txtName;
+    TextView txtPhone;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        txtName = findViewById(R.id.txtName);
+        txtPhone = findViewById(R.id.txtPhone);
+    }
+
+    public void onBtnSubmit(View view) {
+        Toast.makeText(this, txtName.getText().toString() + " " + txtPhone.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
