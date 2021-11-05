cb315eb6404165f2546a351e0c599f635bf6db84
Akos Becsey <becsey.a@gmail.com>
2021-11-05 02:46:30 +0100

    ApiClient - listUsers()

--- app/src/main/java/edu/upm/findme/activities/MenuActivity.java
+++ app/src/main/java/edu/upm/findme/activities/MenuActivity.java
@@ -1,16 +1,37 @@
 package edu.upm.findme.activities;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import edu.upm.findme.R;
+import edu.upm.findme.model.User;
+import edu.upm.findme.utility.ApiClient;
+import edu.upm.findme.utility.UserInfoManager;
 
-public class MenuActivity extends AppCompatActivity {
+public class MenuActivity extends AppCompatActivity implements ApiClient.FailureHandler {
+
+    final ApiClient api = new ApiClient(this);
+    final UserInfoManager userInfo = new UserInfoManager(this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        api.listUsers((users) -> {
+            String name = "";
+            for (User u : users) {
+                if (u.getId() == userInfo.getUserId())
+                    name = u.getName();
+            }
+            Toast.makeText(this, "Hello, " + name + "!\nNumber of users: " + users.size(), Toast.LENGTH_SHORT).show();
+        });
+    }
+
+    @Override
+    public void onApiFailure(String errorDescription) {
+        Toast.makeText(this, "API error: " + errorDescription, Toast.LENGTH_SHORT).show();
     }
 }
