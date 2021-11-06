789b42526b331ca3cf3a60644d1cf9627d1eaa09
Akos Becsey <becsey.a@gmail.com>
2021-11-06 21:05:21 +0100

    FIX: There is only one observer at a given time

--- app/src/main/java/edu/upm/findme/App.java
+++ app/src/main/java/edu/upm/findme/App.java
@@ -6,9 +6,6 @@ import androidx.lifecycle.Lifecycle;
 import androidx.lifecycle.LifecycleEventObserver;
 import androidx.lifecycle.LifecycleOwner;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import edu.upm.findme.utility.MqttTalker;
 import edu.upm.findme.utility.UserInfoManager;
 
@@ -18,7 +15,7 @@ public class App extends Application implements AppEvent.Observer {
     public MqttTalker mqtt;
 
     boolean hasBeenInitialized;
-    List<MortalObserver> observers = new ArrayList<>();
+    MortalObserver currentObserver = null;
 
     public App init() {
         if (!hasBeenInitialized) {
@@ -35,32 +32,18 @@ public class App extends Application implements AppEvent.Observer {
     }
 
     public void addObserver(MortalObserver observer) {
-        if (!isObserverAlreadyAdded(observer)) {
-            observer.getLifecycle().addObserver((LifecycleEventObserver) (source, event) -> {
-                if (event == Lifecycle.Event.ON_DESTROY) {
-                    removeObserver(observer);
-                }
-            });
-            observers.add(observer);
-        }
-    }
-
-    public void removeObserver(MortalObserver observer) {
-        observers.remove(observer);
+        observer.getLifecycle().addObserver((LifecycleEventObserver) (source, event) -> {
+            if (event == Lifecycle.Event.ON_RESUME)
+                currentObserver = observer;
+            else if (event == Lifecycle.Event.ON_PAUSE)
+                currentObserver = null;
+        });
     }
 
     @Override
     public void onGlobalEvent(AppEvent.Type e) {
-        for (MortalObserver o : observers)
-            o.onGlobalEvent(e);
-    }
-
-    boolean isObserverAlreadyAdded(MortalObserver observer) {
-        for (MortalObserver o : observers) {
-            if (o == observer)
-                return true;
-        }
-        return false;
+        if (currentObserver != null)
+            currentObserver.onGlobalEvent(e);
     }
 
     public interface MortalObserver extends AppEvent.Observer, LifecycleOwner {
