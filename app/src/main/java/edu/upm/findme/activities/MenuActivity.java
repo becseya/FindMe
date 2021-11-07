4c3c86720c00566abb77d6a162e7e296d0126723
maryyyyyyaa <76183522+maryyyyyyaa@users.noreply.github.com>
2021-11-07 17:28:15 +0100

    update menu activity
    update MenuActivity.java
    and activity_menu.xml

--- app/src/main/java/edu/upm/findme/activities/MenuActivity.java
+++ app/src/main/java/edu/upm/findme/activities/MenuActivity.java
@@ -2,9 +2,13 @@ package edu.upm.findme.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 import edu.upm.findme.App;
 import edu.upm.findme.AppEvent;
@@ -18,10 +22,29 @@ public class MenuActivity extends AppCompatActivity implements ApiClient.Failure
     final ApiClient api = new ApiClient(this);
     final UserInfoManager userInfo = new UserInfoManager(this);
 
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        listView=(ListView)findViewById(R.id.listViewUser);
+
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("user1");
+        arrayList.add("user2");
+        arrayList.add("user3");
+        arrayList.add("user4");
+        arrayList.add("user5");
+        arrayList.add("user6");
+        arrayList.add("user7");
+        arrayList.add("user8");
+        arrayList.add("user9");
+        arrayList.add("user10");
+
+
+        ArrayAdapter arrayAdapter=new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
 
         api.listUsers((users) -> {
             String name = "";
