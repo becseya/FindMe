ca0939c9f9e73601bb8ddba380a167f534c319c5
maryyyyyyaa <76183522+maryyyyyyaa@users.noreply.github.com>
2021-11-10 20:31:08 +0100

    add MessageViewHolder.java and MessageAdapter.java

---
+++ app/src/main/java/edu/upm/findme/adapters/MessageAdapter.java
@@ -0,0 +1,44 @@
+package edu.upm.findme.adapters;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import edu.upm.findme.R;
+import edu.upm.findme.model.Message;
+
+public class MessageAdapter extends RecyclerView.Adapter<MessageViewHolder> {
+
+    List<Message> messages = null;
+
+    public MessageAdapter() {
+        super();
+    }
+
+    @NonNull
+    @Override
+    public MessageViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_items, parent, false);
+        return new MessageViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MessageViewHolder holder, int position) {
+        holder.bindValues(messages.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return (messages == null) ? 0 : messages.size();
+    }
+
+    public void updateMessages(List<Message> messages) {
+        this.messages = messages;
+        notifyDataSetChanged();
+    }
+}
---
+++ app/src/main/java/edu/upm/findme/adapters/MessageViewHolder.java
@@ -0,0 +1,24 @@
+package edu.upm.findme.adapters;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import edu.upm.findme.R;
+import edu.upm.findme.model.Message;
+
+public class MessageViewHolder extends RecyclerView.ViewHolder {
+
+    TextView content;
+
+    public MessageViewHolder(View itemView) {
+        super(itemView);
+        this.content = itemView.findViewById(R.id.message);
+
+    }
+
+    void bindValues(Message message) {
+        content.setText(message.getContent());
+    }
+}
