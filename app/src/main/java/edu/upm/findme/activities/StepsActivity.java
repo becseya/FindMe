ef38832266307fe1d30580c1331199beabe00808
Akos Becsey <becsey.a@gmail.com>
2021-11-11 16:40:44 +0100

    Live chart

--- app/src/main/java/edu/upm/findme/activities/StepsActivity.java
+++ app/src/main/java/edu/upm/findme/activities/StepsActivity.java
@@ -13,15 +13,20 @@ import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import edu.upm.findme.App;
+import edu.upm.findme.AppEvent;
 import edu.upm.findme.R;
+import edu.upm.findme.model.User;
 
-public class StepsActivity extends AppCompatActivity {
+public class StepsActivity extends AppCompatActivity implements App.MortalObserver {
+
+    private static final int MAX_NUMBER_OF_TOP_USERS = 3;
 
     App app;
-    List<BarEntry> entries;
-    ArrayList<String> labels;
+    List<BarEntry> entries = new ArrayList<>();
+    ArrayList<String> labels = new ArrayList<>();
     BarDataSet dataSetSteps;
     BarData chartData;
     BarChart chartSteps;
@@ -31,18 +36,7 @@ public class StepsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_steps);
 
-        app = ((App) getApplicationContext()).init();
-
-        entries = new ArrayList<>();
-        entries.add(new BarEntry(1, 220));
-        entries.add(new BarEntry(2, 110));
-        entries.add(new BarEntry(3, 40));
-
-        labels = new ArrayList<>();
-        labels.add("Y axis");
-        labels.add("Maryam");
-        labels.add("Ana");
-        labels.add("Ákos");
+        app = ((App) getApplicationContext()).initWithObserver(this);
 
         chartSteps = (BarChart) findViewById(R.id.chartSteps);
         dataSetSteps = new BarDataSet(entries, "Steps");
@@ -51,9 +45,76 @@ public class StepsActivity extends AppCompatActivity {
         chartSteps.setDescription(null);
 
         XAxis xAxis = chartSteps.getXAxis();
-        xAxis.setValueFormatter(new IndexAxisValueFormatter(labels));
         xAxis.setGranularity(1f);
         xAxis.setGranularityEnabled(true);
         xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateUI();
+    }
+
+    void updateUI() {
+        calculateTopUsers();
+        dataSetSteps.notifyDataSetChanged();
+        chartData.notifyDataChanged();
+        chartSteps.notifyDataSetChanged();
+        chartSteps.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
+        chartSteps.invalidate();
+    }
+
+    void updateTopUsers(List<TopEntry> list, TopEntry candidate) {
+        if (list.size() == 0)
+            list.add(candidate);
+        else {
+            for (int i = 0; i < list.size(); i++) {
+                if (candidate.steps > list.get(i).steps) {
+                    list.add(i, candidate);
+                    break;
+                }
+            }
+            if (list.size() < MAX_NUMBER_OF_TOP_USERS)
+                list.add(candidate);
+            else if (list.size() > MAX_NUMBER_OF_TOP_USERS)
+                list.remove(list.size() - 1);
+        }
+    }
+
+    void calculateTopUsers() {
+        List<TopEntry> top3users = new ArrayList<>();
+
+        for (Map.Entry<Integer, Integer> entry : app.mqtt.getSteps().entrySet()) {
+            User user = User.getById(app.users, entry.getKey());
+
+            if (user != null)
+                updateTopUsers(top3users, new TopEntry(entry.getKey(), entry.getValue(), user.getName()));
+        }
+
+        labels.clear();
+        entries.clear();
+        for (int i = 0; i < top3users.size(); i++) {
+            entries.add(new BarEntry(i, top3users.get(i).steps));
+            labels.add(top3users.get(i).name);
+        }
+    }
+
+    @Override
+    public void onGlobalEvent(AppEvent.Type e) {
+        if (e.equals(AppEvent.Type.STEP_SCORES_CHANGED))
+            updateUI();
+    }
+
+    private static class TopEntry {
+        public int id;
+        public int steps;
+        public String name;
+
+        public TopEntry(Integer id, Integer steps, String name) {
+            this.id = id;
+            this.steps = steps;
+            this.name = name;
+        }
     }
 }
--- app/src/main/java/edu/upm/findme/model/User.java
+++ app/src/main/java/edu/upm/findme/model/User.java
@@ -1,5 +1,7 @@
 package edu.upm.findme.model;
 
+import java.util.List;
+
 public class User {
     int id;
     String name;
@@ -9,6 +11,14 @@ public class User {
         this.id = id;
         this.name = name;
         this.phoneNumber = phoneNumber;
+    }
+
+    static public User getById(List<User> users, int id) {
+        for (User u : users)
+            if (u.getId() == id)
+                return u;
+
+        return null;
     }
 
     public String getName() {
